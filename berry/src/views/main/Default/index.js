3957c57cedc8a3d2d267be92174866426c2d39c4
Ahat <ahatahat123@gmail.com>
2021-07-16 11:21:02 +0900

    메인페이지 접속 시 플레이리스트를 받아오도록 수정

--- berry/src/views/main/Default/index.js
+++ berry/src/views/main/Default/index.js
@@ -1,54 +1,18 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 
 import MainCard from './../../../ui-component/cards/MainCard';
 import { makeStyles } from '@material-ui/styles';
 import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, Select, MenuItem, InputLabel, Button, Grid } from '@material-ui/core';
 
 const Main = () => {
-    const loadingPlaylist = () => {
-        const weekArr = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
-        const result = [];
-        for (let i = 0; i < weekArr.length; i++) {
-            result.push(<MenuItem key={i} value={weekArr[i]}>{weekArr[i]}</MenuItem>);
-        }
-        return result;
-    };
+  const axios = require('axios');
 
-    const useStyles = makeStyles((theme) => ({
-        formControl: {
-          margin: theme.spacing(1),
-          minWidth: 120,
-        },
-        selectEmpty: {
-          marginTop: theme.spacing(2),
-        },
-        paper: {
-          padding: theme.spacing(2),
-          textAlign: 'center',
-          color: 'blue',
-          backgroundColor: 'red'
-        },
-        frame: {
-            height: 'calc(100vh - 210px)',
-            border: '1px solid',
-            borderColor: theme.palette.primary.light
-        },
-        area: {
-            height: 'calc(80vh - 210px)',
-            borderColor: theme.palette.primary.light
-        },
-        test: {
-            textAlign: 'center',
-            width: '100%'
-        }
-      }));
-    
-  const classes = useStyles();
-  const [type, setType] = React.useState('playlist');
-  const [item, setItem] = React.useState('path');
-  const [playlist, setPlaylist] = React.useState("Select Playlist");
-  const [order, setOrder] = React.useState("asc");
-
+  const [type, setType] = useState('playlist');
+  const [item, setItem] = useState('path');
+  const [playlist, setPlaylist] = useState("Select Playlist");
+  const [playlists, setPlaylists] = useState([]);
+  const [order, setOrder] = useState("asc");
+  
   const handleChangeType = (event) => {
     setType(event.target.value);
   };
@@ -62,70 +26,118 @@ const Main = () => {
     setOrder(event.target.value);
   };
 
-    return (
-        <MainCard  className={classes.frame} title="Sort Your Music">
-            
-        <Grid container spacing={3} className={classes.area}>
-            <Grid item xs={12}>
-            <FormControl>
-            <FormLabel component="legend">Type</FormLabel>
-            <RadioGroup row aria-label="position" name="position" defaultValue="top" value={type} onChange={handleChangeType}>
-                <FormControlLabel value="playlist" control={<Radio selected/>} label="Playlist" />
-                <FormControlLabel value="star" control={<Radio />} label="Star" />
-            </RadioGroup>
-            </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-            <InputLabel id="demo-simple-select-helper-label1">Playlist</InputLabel>
-            <FormControl className={classes.test}>
-                <Select
-                id="demo-simple-select1"
-                labelId="demo-simple-select-helper-label1"
-                value={playlist}
-                onChange={handleChangePlaylist}
-                inputProps={{ 'aria-label': 'Without label' }}
-                >
-                <MenuItem value="Select Playlist" disabled>Select Playlist</MenuItem>
-                {loadingPlaylist()}
-                </Select>
-            </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-            <InputLabel id="demo-simple-select-helper-label2">Sort Item</InputLabel>
-            <FormControl className={classes.test}>
-                <Select
-                id="demo-simple-select2"
-                labelId="demo-simple-select-helper-label2"
-                value={item}
-                onChange={handleChangeSortItem}
-                inputProps={{ 'aria-label': 'Without label' }}
-                >
-                <MenuItem value='path'>Path</MenuItem>
-                <MenuItem value='title'>Title</MenuItem>
-                <MenuItem value='album'>Album</MenuItem>
-                <MenuItem value='artist'>Artist</MenuItem>
-                <MenuItem value='year'>Year</MenuItem>
-                <MenuItem value='genre'>Genre</MenuItem>
-                <MenuItem value='size'>Size</MenuItem>
-                </Select>
-            </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-            <FormControl>
-                <FormLabel component="legend">Order</FormLabel>
-                <RadioGroup row aria-label="position" name="position" defaultValue="top" value={order} onChange={handleChangeOrder}>
-                    <FormControlLabel value="asc" control={<Radio selected/>} label="Asc" />
-                    <FormControlLabel value="desc" control={<Radio />} label="Desc" />
-                </RadioGroup>
-            </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-            <Button variant="contained" className={classes.test}>Sort Start!</Button> 
-            </Grid>
-        </Grid>
-        <hr></hr>
-        </MainCard>
-    );
+  const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: 'blue',
+      backgroundColor: 'red'
+    },
+    frame: {
+        height: 'calc(100vh - 210px)',
+        border: '1px solid',
+        borderColor: theme.palette.primary.light
+    },
+    area: {
+        height: 'calc(80vh - 210px)',
+        borderColor: theme.palette.primary.light
+    },
+    test: {
+        textAlign: 'center',
+        width: '100%'
+    }
+  }));
+  const classes = useStyles();
+  
+
+  const loadingPlaylist = () => {
+    axios.get('http://127.0.0.1:8080/getplaylist').then(
+      (response) => {        
+        const words = response.data.split(',');
+        setPlaylists(words);
+      }
+    ).catch(error => {
+        throw(error);
+    })
+  };
+  useEffect(() => {
+    loadingPlaylist();
+  }, []);
+
+  return (
+    <MainCard  className={classes.frame} title="Sort Your Music">
+    <Grid container spacing={3} className={classes.area}>
+      <Grid item xs={12}>
+      <FormControl>
+      <FormLabel component="legend">Type</FormLabel>
+      <RadioGroup row aria-label="position" name="position" defaultValue="top" value={type} onChange={handleChangeType}>
+        <FormControlLabel value="playlist" control={<Radio selected/>} label="Playlist" />
+        <FormControlLabel value="star" control={<Radio />} label="Star" />
+      </RadioGroup>
+      </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+      <InputLabel id="demo-simple-select-helper-label1">Playlist</InputLabel>
+      <FormControl className={classes.test}>
+        <Select
+        id="demo-simple-select1"
+        labelId="demo-simple-select-helper-label1"
+        value={playlist}
+        onChange={handleChangePlaylist}
+        inputProps={{ 'aria-label': 'Without label' }}
+        >
+        <MenuItem value="Select Playlist" disabled>Select Playlist</MenuItem>
+        {playlists.map((item, i) => (
+          <MenuItem key={i} value={item}>
+            {item}
+          </MenuItem>
+        ))}
+        </Select>
+      </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+      <InputLabel id="demo-simple-select-helper-label2">Sort Item</InputLabel>
+      <FormControl className={classes.test}>
+          <Select
+          id="demo-simple-select2"
+          labelId="demo-simple-select-helper-label2"
+          value={item}
+          onChange={handleChangeSortItem}
+          inputProps={{ 'aria-label': 'Without label' }}
+          >
+          <MenuItem value='path'>Path</MenuItem>
+          <MenuItem value='title'>Title</MenuItem>
+          <MenuItem value='album'>Album</MenuItem>
+          <MenuItem value='artist'>Artist</MenuItem>
+          <MenuItem value='year'>Year</MenuItem>
+          <MenuItem value='genre'>Genre</MenuItem>
+          <MenuItem value='size'>Size</MenuItem>
+          </Select>
+      </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+      <FormControl>
+          <FormLabel component="legend">Order</FormLabel>
+          <RadioGroup row aria-label="position" name="position" defaultValue="top" value={order} onChange={handleChangeOrder}>
+              <FormControlLabel value="asc" control={<Radio selected/>} label="Asc" />
+              <FormControlLabel value="desc" control={<Radio />} label="Desc" />
+          </RadioGroup>
+      </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+      <Button variant="contained" className={classes.test}>Sort Start!</Button> 
+      </Grid>
+    </Grid>
+    <hr></hr>
+    </MainCard>
+  );
 };
 
 export default Main;
--- web.go
+++ web.go
@@ -3,26 +3,46 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"regexp"
 )
 
-func indexTandler(w http.ResponseWriter, r *http.Request) {
-	//전달할 http 내용
-	fmt.Fprintf(w, "123456")
+func getPlaylistHandler(w http.ResponseWriter, r *http.Request) {
+	requester, err := NewSonicRequester()
+	if err != nil {
+		fmt.Println("ERROR : ", err)
+	}
+
+	result := requester.GetPlaylists()
+	playlists := ""
+	for _, item := range result.SubsonicResponse.Playlists.Playlist {
+		playlists += "," + item.Name
+	}
+	if len(playlists) != 0 {
+		playlists = playlists[1:]
+	}
+
+	fmt.Fprintf(w, playlists)
+}
+
+func sortHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "2")
 }
 
 func webMain() {
-	//웹서버 URL 리퀘스트 받기
-	http.HandleFunc("/test", indexTandler)
+	http.HandleFunc("/getplaylist", getPlaylistHandler)
+	http.HandleFunc("/sort", sortHandler)
 
-	//파일서버 이거 어떻게 못하나 이렇게 해야 에러가 안나네
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("public"))))
-	http.Handle("/main/default", http.StripPrefix("/main/default", http.FileServer(http.Dir("public"))))
-	http.Handle("/static", http.StripPrefix("/static", http.FileServer(http.Dir("public"))))
-
-	//서버 실행
+	fileServer := http.FileServer(http.Dir("public"))
+	fileMatcher := regexp.MustCompile(`\.[a-zA-Z]*$`)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if !fileMatcher.MatchString(r.URL.Path) {
+			http.ServeFile(w, r, "public/index.html")
+		} else {
+			fileServer.ServeHTTP(w, r)
+		}
+	})
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println(err)
 	}
-
 }
